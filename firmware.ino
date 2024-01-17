678872e3ab8dc358d319b8d988be8a7a6d602b35
DeadMakar90 <deadmakar90@yandex.ru>
2024-01-18 00:14:11 +0300

    first_commit

---
+++ firmware.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>               //библиотека для i2c
+#include <LiquidCrystal_I2C.h>  //библиотека для дисплея
+
+LiquidCrystal_I2C lcd(0x27,16,2);   //инициализация дисплея
+
+int realy1 = 3;              //пин реле 3
+int realy2 = 4;              //пин реле 4
+
+void setup()
+{
+//    Serial.begin(9600);             //инциализация монитора com-порта для отладки
+    pinMode(realy1,OUTPIT);
+    pinMode(realy2,OUTPIT);
+}
+void loop()
+{
+
+}
+
+void relay_time(){                      //функция управления лампами
+
+}
+
+void relay_temp(){                      //функция управления полдогревом
+
+}
+
+void lcd_out(){                         //функция вывода на экран
+
+}
