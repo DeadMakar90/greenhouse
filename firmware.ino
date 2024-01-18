2161b1276ce5f126d5f8492d469e7cc2edc690ec
DeadMakar90 <deadmakar90@yandex.ru>
2024-01-18 22:20:20 +0300

    update_18.01.24

--- firmware.ino
+++ firmware.ino
@@ -3,23 +3,25 @@
 #include <DHT_U.h>              //библиотека для датчика DHT
 #include <Wire.h>               //библиотека для i2c
 #include <LiquidCrystal_I2C.h>  //библиотека для дисплея 
-#define DHTPIN1 3                   // пин датчика DTH11 d5
-#define DHTPIN2 2                   // пин датчика DTH11 d2
-LiquidCrystal_I2C lcd(0x27,16,2);   //инициализация дисплея
-iarduino_RTC watch(RTC_DS3231);     //инициализация часов 
+#define DHTPIN1 2                   // пин датчика DTH11 d5
+#define DHTPIN2 3                   // пин датчика DTH11 d2
+LiquidCrystal_I2C lcd(0x27,16,2);   //установка экрана
+iarduino_RTC watch(RTC_DS3231);     //установка часов 
 uint32_t timer1,timer2,timer3;      //таймеры для выполения функций
-int realy1 = 5;              //пин реле d3
-int realy2 = 4;              //пин реле d4
+int relay1 = 4;              //пин реле d3
+int relay2 = 5;              //пин реле d4
 
 void setup()
 {
 //    Serial.begin(9600);             //инциализация монитора com-порта для отладки
-    pinMode(realy1,OUTPIT);
-    pinMode(realy2,OUTPIT);
-     watch.begin();
+    pinMode(relay1,OUTPUT);
+    pinMode(relay2,OUTPUT);
+    watch.begin();                      //инциализация часов
     //watch.settime(__TIMESTAMP__);     //Установка времени в модуль, в таком описании равен времени компиляции кода, т.е +- вермя на ПК. После первой прошивки закоммпентировать!
-    lcd.init();                     
-    lcd.backlight();
+    lcd.init();                         //инциализация экрана           
+    lcd.backlight();                    //включить подсветку экрана
+    digitalWrite(relay1, HIGH);
+    digitalWrite(relay2, HIGH);
 }
 void loop()
 {
@@ -57,28 +59,28 @@ void relay_time(){                      //функция управления л
   }
 }
 void relay_temp(){                      //функция управления полдогревом
-  float temp1 = dht1.readTemperature(DHTPIN1);      //данные с датчика 1
-  float temp2 = dht2.readTemperature(DHTPIN2);      //данные с датчика 2
+  float temp1 = dht1.readTemperature();      //данные с датчика 1
+  float temp2 = dht2.readTemperature();      //данные с датчика 2
   float up_lemit  = 24.0;                           //верхний порог температуры
   float down_lemit = 20.0;                          //нижний порог температуры
  // Serial.println(temp1);                          //выхлоп в монитор порта для отладки
  // Serial.println(temp2);                          //выхлоп в монитор порта для отладки
  if (temp1 || temp2 >= up_lemit)
  {
-    dititalWrite(realy2,HIGH);
+    dititalWrite(relay2,HIGH);
  }
  if (temp1 || temp2 <= down_lemit)
  {
-    digitalWrite(realy2,LOW);
+    digitalWrite(relay2,LOW);
  }
 }
 void lcd_out(){                         //функция вывода на экран
-  float temp1 = dht1.readTemperature(DHTPIN1);      
-  float temp2 = dht2.readTemperature(DHTPIN2);
+  float temp1 = dht1.readTemperature();      
+  float temp2 = dht2.readTemperature();
   lcd.setCursor(0,0);                     // Устанавливаем курсор на первую строку и нулевой символ.
   lcd.print(watch.gettime("H:i:s"));
   lcd.setCursor(0, 1);
   lcd.print(temp1);
-  lcd.setCursor(5, 1);
+  lcd.setCursor(8, 1);
   lcd.print(temp2);
 }
